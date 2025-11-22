6eef6c4581d68855eafcc0be2263bc44c1d0267b
Samrat-Debugger <md721023@gmail.com>
2025-11-23 01:20:12 +0600

    button activated

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 let button = document.getElementById("button");
+isRed = false;
+
 button.addEventListener("click", function () {
-    document.body.style.backgroundColor = "red"
-});
+    if (isRed) {
+        document.body.style.backgroundColor = "white";
+        isRed = false;
+    }
+    else {
+        document.body.style.backgroundColor = "red";
+        isRed = true;
+    }
+})
